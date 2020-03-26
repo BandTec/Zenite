2916f9f985669d22c824af1d0431ed5f5a11cdf2
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2020-03-26 12:33:49 -0300

    [feat] menu lateral

---
+++ Dev/frontend/src/components/MenuLateral/BotaoMenu/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Onibus from '../../../assets/icons/onibus.svg';
+import Linha from '../../../assets/icons/linha.svg';
+import Fiscal from '../../../assets/icons/fiscal.svg';
+import Motorista from '../../../assets/icons/motorista.svg';
+import Perfil from '../../../assets/icons/config.svg';
+import Logout from '../../../assets/icons/logout.svg';
+import LogoIcone from '../../../assets/logos/favicon4.png';
+
+import { Botao, Icone, Texto } from './styles';
+
+export default function BotaoMenu({ iconeNome, descricao, url, alt, ativo=false } ) {
+
+  const iconesLib = {
+    onibus: Onibus,
+    linha: Linha,
+    fiscal: Fiscal,
+    motorista: Motorista,
+    perfil: Perfil,
+    logout: Logout,
+    logo: LogoIcone
+  }
+
+  return (
+   <Botao href={url} ativo={ativo}>
+      <Icone src={iconesLib[iconeNome]} alt={alt} />
+     <Texto>{descricao}</Texto>
+    </Botao>
+  );
+}
