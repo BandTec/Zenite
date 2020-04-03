060c65a69cb9d411c14b204674a71f6ad24ecd8d
Vitor Silva <vitor.procont@gmail.com>
2020-04-03 16:34:33 -0300

    cookie funcionando

--- Dev/api-olhovivo/app.js
+++ Dev/api-olhovivo/app.js
@@ -6,6 +6,20 @@ const banco = require('./conexaoBanco');
 
 const url = "http://api.olhovivo.sptrans.com.br/v2.1"
 const tokenApi = "68b21b2faf9ec3708c35f900fa3d7a3cc735ee4c0bddcfe79fff54ba8dadd79b";
+let cookie;
+
+//NÃO OUSAR TOCAR NESSA PARTE
+//****************************************************
+const terminais = ["CID. TIRADENTES","METRÔ ITAQUERA","CARRÃO",
+"BRITANIA","PIRITUBA",
+"CASA VERDE","CACHOEIRINHA",
+"CARVALHO","ARICANDUVA","PENHA","SÃO MIGUEL",
+"SÃO MATEUS","SACOMÃ","SAPOPEMBA",
+"GRAJAÚ","PARELHEIROS","VARGINHA",
+"ÁGUA ESPRAIADA","CAPELINHA","GUARAPIRANGA","JARDIM ÂNGELA","JOÃO DIAS","STO. AMARO",
+"CAMPO LIMPO",
+"AMARAL GURGEL","BANDEIRA","LAPA","MERCADO","TERM. PQ. D. PEDRO II","PINHEIROS","PRINC. ISABEL","VL PRUDENTE"]
+//****************************************************
 
 app.use(express.json());
 app.listen(3000, () => {
@@ -20,8 +34,13 @@ async function autenticar() {
     try{
         let autenticado = false;
         do{
-            const response = await axios.post(`${url}/Login/Autenticar?token=${tokenApi}`);
-            autenticado = response.data;
+            const res = await axios.post(`${url}/Login/Autenticar?token=${tokenApi}`);
+            autenticado = true;
+
+            cookie = res.headers["set-cookie"];
+            let aux = cookie[0].split(";");
+            cookie = aux[0];
+
         }while(autenticado==false);
         console.log("Autenticado");
     }catch(err){
@@ -32,15 +51,29 @@ async function autenticar() {
 app.get("/cadastrarlinhas", async (req, res) => {
     try{
         await autenticar();
-        const response = [];
+        const linhas = [];
 
-        for (let posicao = 0; posicao < 9999; posicao++) {
-            response.push(await axios.get(`${url}/Linha/Buscar?termosBusca=${posicao}`));
-        }
-
-        res.json(response);
+        await terminais.map(async item => {
+            const response = await axios.get(`${url}/Linha/Buscar?termosBusca=${item}`, 
+            {headers: {Cookie: cookie}});
+            linhas.push(response.data)
+            console.log(response.data)
+        });
+        res.json(linhas);
     }catch(err){
         console.log(err);
         res.json(err.message);
+    }
+});
+
+app.get("/teste", async (req, res) => {
+    try{
+        await autenticar();
+        const response = await axios.get(`${url}/Linha/Buscar?termosBusca=VL PRUDENTE`, 
+            {headers: {Cookie: cookie}});
+        res.status(200).json(response.data);
+    }catch(err){
+        console.log(err);
+        res.status(500).json(err.message);
     }
 });
