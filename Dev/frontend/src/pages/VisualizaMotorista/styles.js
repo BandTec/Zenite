3a4e3ef81923d68f9794f5e261f421f1ac5cbcaf
unknown <rayssa.hcl@gmail.com>
2020-04-08 23:27:02 -0300

    Adicionei página visualiza motorista

---
+++ Dev/frontend/src/pages/VisualizaMotorista/styles.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #ffffff;
+  display: flex;
+`;
+
+export const Row = styled.div`
+  display: block;
+  width: 100%;
+  margin-top: 20px;
+  `;
+
+export const Cabecalho = styled.div`
+  display: block;
+  margin-left: 110px;
+`;
+
+export const BotaoRelatorio = styled.div`
+  display: block;
+  margin-right: 5vh;
+`;
+
+export const BotaoNovoMotorista = styled.div`
+  display: block;
+  margin-left: 15vh;
+`;
+
+export const Tela = styled.div`
+  width: 100%;
+`;
+
+export const Acoes = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const CaixaTabela = styled.div`
+  display: block;
+  margin-left: 15vh;
+  margin-right: 5vh;
+`;
