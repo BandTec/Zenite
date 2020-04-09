f92cfe210353015665302da5d887bad903a3bc61
unknown <rayssa.hcl@gmail.com>
2020-04-08 23:23:39 -0300

    Adicionei rota do motorista

--- Dev/frontend/src/App.js
+++ Dev/frontend/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Login from "./pages/Login";
 import Fiscal from "./pages/VisualizaFiscal";
 import Linha from "./pages/VisualizaLinha";
+import Motorista from "./pages/VisualizaMotorista";
 import Dashboard from "./pages/Dashboard";
 import DetalhesFiscal from "./pages/DetalhesFiscal";
 import FiscalCadastro1 from "./pages/CadastroFiscal/dadosPessoais";
@@ -54,7 +55,7 @@ const App = () => {
 
         {/* MOTORISTA */}
 
-        {/* <Route exact path="/motorista" component={} /> */}
+         <Route exact path="/motorista" component={Motorista} /> 
 
         {/* <Route path="/motorista/detalhes/:id" component={} /> */}
 
