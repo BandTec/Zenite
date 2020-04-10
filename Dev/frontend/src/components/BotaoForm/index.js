bb5d1373b19d80250ce60935c02b831f5f911225
jPedroSoares <joao.soares204@hotmail.com>
2020-04-10 15:53:41 -0300

    [melhoria] Adicionando função padrão

--- Dev/frontend/src/components/BotaoForm/index.js
+++ Dev/frontend/src/components/BotaoForm/index.js
@@ -4,7 +4,7 @@ import { Container, Icone, Texto } from './styles';
 // import Seta from "../../assets/icons/setaOval.png";
 import Check from "../../assets/icons/check.png";
 
-export default function BotaoForm({texto, ladoDireito = true, concluir = false, invisivel= false, mudarPagina }) {
+export default function BotaoForm({texto, ladoDireito = true, concluir = false, invisivel= false, mudarPagina = () => {} }) {
   return (
     <Container ladoDireito={ladoDireito} invisivel={invisivel} 
       onClick={() => mudarPagina(ladoDireito)}>
