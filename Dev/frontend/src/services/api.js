94acfec45b19a66b0c5b39dd175fe3f9cc9910f5
jPedroSoares <joao.soares204@hotmail.com>
2020-05-25 09:50:47 -0300

    backend

--- Dev/frontend/src/services/api.js
+++ Dev/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://backendzenite.azurewebsites.net',
+    baseURL: 'https://backendzenite.azurewebsites.net',
 })
 
 export default api;
