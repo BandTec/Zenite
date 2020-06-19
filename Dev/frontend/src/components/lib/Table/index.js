1ce53847809174756d5c73cf6dd2e4f91ccccf1c
jPedroSoares <joao.soares204@hotmail.com>
2020-06-18 22:08:54 -0300

    Adicionada pesquisa em tabela da dashboard

--- Dev/frontend/src/components/lib/Table/index.js
+++ Dev/frontend/src/components/lib/Table/index.js
@@ -1,23 +1,43 @@
 import React from 'react';
 
 import { Container, Titulo, Busca, Tabela } from './styles';
+import { useState, useEffect } from 'react';
 
-function Table({titulo = "Tabela", placeholder="Pesquise...", dados}) {
+function Table({titulo = "Tabela", placeholder="Pesquise...", dados = {header: [], body: []}}) {
+    const [dadosTabela, setDadosTabela] = useState({header: [], body: []});
+    useEffect(() => {
+        setDadosTabela(dados)
+    },[dados])
+
+    const search = event => {
+        let aux = []
+        if(event.target.value){
+            for(const body of dados.body)
+                for(const dado of body)
+                    if(typeof dado === 'string' && dado.indexOf(event.target.value) != -1)
+                        aux.push(body)
+            setDadosTabela({header: dadosTabela.header, body: aux})
+            aux = []
+        }else{
+            setDadosTabela(dados)
+        }
+    }
+
     return (
       <Container>
           <Titulo>{titulo}</Titulo>
-          <Busca placeholder={placeholder}/>
-          { dados ?
+          <Busca placeholder={placeholder} onChange={e => search(e)}/>
+          { dadosTabela ?
             <Tabela>
                 <thead>
                     <tr>
-                        {dados.header.map(colunaCabecalho => 
+                        {dadosTabela.header.map(colunaCabecalho => 
                                <th>{colunaCabecalho}</th>
                         )}
                     </tr>
                 </thead>
                 <tbody>
-                    {dados.body.map(linha => 
+                    {dadosTabela.body.map(linha => 
                         <tr>{
                             linha.map(coluna => 
                                 <td>{coluna}</td>
