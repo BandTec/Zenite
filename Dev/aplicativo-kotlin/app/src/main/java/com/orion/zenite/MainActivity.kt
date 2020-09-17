31235055d9f4dcaabce3217b8242889f0dc742e9
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2020-09-16 23:29:08 -0300

    organizando e adicionando bases de componentes

--- Dev/aplicativo-kotlin/app/src/main/java/com/orion/zenite/MainActivity.kt
+++ Dev/aplicativo-kotlin/app/src/main/java/com/orion/zenite/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.orion.zenite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.orion.zenite.fiscal.FiscalLinhas
+import com.orion.zenite.fiscal.MainFiscal
+import com.orion.zenite.motorista.MainMotorista
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goDash(view: View){
+        val campoTexto = entrar.text;
+        if(campoTexto.isNotEmpty()){
+            if(campoTexto.toString().toLowerCase().equals("motorista")){
+                val intent = Intent(this@MainActivity, MainMotorista::class.java)
+                startActivity(intent)
+            }else {
+                val intent = Intent(this@MainActivity, MainFiscal::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    fun testeOutraTela(view: View) {
+        val intent = Intent(this@MainActivity, FiscalLinhas::class.java)
+        startActivity(intent)
     }
 }
---
+++ Dev/aplicativo-kotlin/app/src/main/java/com/orion/zenite/fiscal/FiscalLinhas.kt
@@ -0,0 +1,20 @@
+package com.orion.zenite.fiscal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.orion.zenite.R
+import kotlinx.android.synthetic.main.activity_main_fiscal.*
+
+class FiscalLinhas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fiscal_linhas)
+
+        //        https://material.io/develop/android/components/app-bars-top
+        topAppBar.setNavigationOnClickListener {
+            this.finish()
+        }
+    }
+
+}
