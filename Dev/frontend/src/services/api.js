2e075a87d90820532c88c506794c9fcb2c26d59e
João Pedro Soares <39804819+jPedroSoares@users.noreply.github.com>
2020-09-22 15:06:37 -0300

    Update api.js

--- Dev/frontend/src/services/api.js
+++ Dev/frontend/src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: "https://zenitebackend.azurewebsites.net",
+  baseURL: "http://3.86.5.222:8080",
+  // baseURL: "https://zenitebackend.azurewebsites.net",
 });
 
 export default api;
