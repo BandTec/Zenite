4df5d681abdb95989deed92392c4d07f47ff54cd
Fabi <fabicaneyu@gmail.com>
2020-10-11 11:15:16 -0300

    TELAS FINALIZADAS: Motorista Dash, Motorista tela Qrcode, Logout

---
+++ Dev/aplicativo-kotlin/app/src/main/java/com/orion/zenite/activity_logout.kt
@@ -0,0 +1,22 @@
+package com.orion.zenite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class activity_logout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logout)
+    }
+
+
+    fun sairConta(component: View){
+
+    }
+
+
+    fun suporte(component: View){
+
+    }
+}
