e498f89d99935d1fd1ab9405c22a369307d38b32
Raissa Arantes <rayssa.hcl@gmail.com>
2020-10-21 20:09:30 -0200

    Criei os models

--- Dev/aplicativo-kotlin/app/src/main/java/com/orion/zenite/model/Conta.kt
+++ Dev/aplicativo-kotlin/app/src/main/java/com/orion/zenite/model/Conta.kt
@@ -1,7 +1,8 @@
 package com.orion.zenite.model
 
+
 data class Conta (
-    val idConta: Int,
+    val idConta: Int?,
     val senha: String,
     val email: String,
     val nivel: Nivel
@@ -11,3 +12,4 @@ data class Nivel(
     val id: Int,
     val descricao: String
 )
+
