f8bdc94623ee7133f47d47e21fba4eb7c4fb1b51
Raissa Arantes <rayssa.hcl@gmail.com>
2020-10-21 23:17:28 -0200

    Criei uma clase model para o Token

--- Dev/aplicativo-kotlin/app/src/main/java/com/orion/zenite/model/LoginResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.orion.zenite.model
-
-data class LoginResponse (
-    val email: String,
-    val senha: String
-)
-
-
---
+++ Dev/aplicativo-kotlin/app/src/main/java/com/orion/zenite/model/Token.kt
@@ -0,0 +1,6 @@
+package com.orion.zenite.model
+
+data class Token(
+    val message: String, // "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmZXJuYW5kYS5lc3RldmVzQHplbml0ZS5jb20uYnIiLCJleHAiOjM3ODkxMzIwOTExfQ.xVdfEOI0Lids6FmdKQppsK9eQgnc4_RaTJt2icLAoWY"
+    val value: Boolean?
+)
