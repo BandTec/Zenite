2fe2b3be7b5e6cea90d8301f547587c69102f075
Raissa Arantes <rayssa.hcl@gmail.com>
2020-11-16 21:53:50 -0200

    Alterar Intervalo

--- Dev/aplicativo-kotlin/app/src/main/java/com/orion/zenite/http/fiscal/FiscalApi.kt
+++ Dev/aplicativo-kotlin/app/src/main/java/com/orion/zenite/http/fiscal/FiscalApi.kt
@@ -14,38 +14,70 @@ interface FiscalApi {
 
     // TODAS AS LINHAS DO FISCAL X
     @GET("/api/fiscal/{id}/linhas")
-    fun getFiscalLinhas(@Path("id") id: Int, @Header("authorization") auth: String): Call<List<Linha>>
+    fun getFiscalLinhas(
+        @Path("id") id: Int,
+        @Header("authorization") auth: String
+    ): Call<List<Linha>>
 
     // MOTORISTAS E ONIBUS DA LINHA Y
     @GET("/api/linha/{id}/onibus")
-    fun getLinhaMotoristaOnibus(@Path("id") id: Int, @Header("authorization") auth: String): Call<List<Onibus>>
+    fun getLinhaMotoristaOnibus(
+        @Path("id") id: Int,
+        @Header("authorization") auth: String
+    ): Call<List<Onibus>>
 
     // iniciar viagem
     @POST("/api/viagem")
-    fun iniciarViagem(@Body info: IniciarViagem,
-                      @Header("authorization"
-                      ) auth: String) : Call<Void>
+    fun iniciarViagem(
+        @Body info: IniciarViagem,
+        @Header(
+            "authorization"
+        ) auth: String
+    ): Call<Void>
 
     // finalizar viagem
     @PUT("/api/viagem/{idViagem}/{idFiscal}")
-    fun finalizarViagem(@Path("idViagem") idViagem: Int,
-                        @Path("idFiscal") idFiscal: Int,
-                        @Header("authorization"
-                    ) auth: String): Call<Void>
+    fun finalizarViagem(
+        @Path("idViagem") idViagem: Int,
+        @Path("idFiscal") idFiscal: Int,
+        @Header(
+            "authorization"
+        ) auth: String
+    ): Call<Void>
 
     // TODO => CAMERA => ABRIR E FECHAR VIAGEM
 
 
     // ADICIONAR QTD PASSAGEIROS
     @PUT("/api/viagem/{idViagem}/qtdPassageiros")
-    fun adicionarPassageiros(@Path("idViagem") idViagem: Int,
-                             @Body qtdPassageiros: QtdPassageiros,
-                             @Header("authorization") auth: String
-                            ) : Call<Void>
+    fun adicionarPassageiros(
+        @Path("idViagem") idViagem: Int,
+        @Body qtdPassageiros: QtdPassageiros,
+        @Header("authorization") auth: String
+    ): Call<Void>
 
     @GET("/api/horarios/linha/{id}")
-    fun getLinhaCronograma(@Path("id") id: Int, @Header("authorization") auth: String): Call<List<Cronograma>>
+    fun getLinhaCronograma(
+        @Path("id")
+        id: Int,
+        @Header("authorization")
+        auth: String
+    ): Call<List<Cronograma>>
 
     @GET("/api/horarios/fiscal/{id}/cronograma/proximahora")
-    fun getCronogramaGeral(@Path("id") id: Int, @Header("authorization") auth: String): Call<List<CronogramaGeral>>
+    fun getCronogramaGeral(
+        @Path("id") id: Int,
+        @Header("authorization")
+        auth: String
+    ): Call<List<CronogramaGeral>>
+
+
+    //ALTERAR INTERVALO DE VIAGEM
+    @POST("api/horarios/alterados/cronograma/{idLinha}/novoIntervalo")
+    fun alterarIntervaloViagem(
+        @Path("idViagem") idViagem: Int,
+        @Body novoIntervalo: NovoIntervalo,
+        @Header("authorization") auth: String
+    ): Call<Void>
+
 }
--- Dev/aplicativo-kotlin/app/src/main/java/com/orion/zenite/telas/fiscal/AlterarIntervalo.kt
+++ Dev/aplicativo-kotlin/app/src/main/java/com/orion/zenite/telas/fiscal/AlterarIntervalo.kt
@@ -3,11 +3,23 @@ package com.orion.zenite.telas.fiscal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.MutableLiveData
 import com.orion.zenite.R
+import com.orion.zenite.http.HttpHelper
+import com.orion.zenite.http.fiscal.FiscalApi
+import com.orion.zenite.model.NovoIntervalo
 import com.orion.zenite.utils.AppPreferencias
+import kotlinx.android.synthetic.main.activity_alterar_intervalo.*
 import kotlinx.android.synthetic.main.activity_cronograma_linha.*
+import kotlinx.android.synthetic.main.activity_cronograma_linha.topAppBar
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 class AlterarIntervalo : AppCompatActivity() {
+    val loading = MutableLiveData<Boolean>()
+    val respostaRequisicao = MutableLiveData<Boolean>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alterar_intervalo)
@@ -24,7 +36,48 @@ class AlterarIntervalo : AppCompatActivity() {
     }
 
     fun alterarIntervalo(component:View) {
-        //alterarIntervalo
-        this.finish()
+
+        val intervalo = input_novo_intervalo.text.toString().trim()
+
+        if (intervalo.isBlank()) {
+            input_novo_intervalo.error = "Informe o novo intervalo"
+            input_novo_intervalo.requestFocus()
+
+        } else {
+            alterar()
+        }
+        //this.finish()
     }
-}
+        fun alterar() {
+            val token =
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1AYWRtLmNvbS5iciIsImV4cCI6Mzc4ODAyNTM3MzV9.Tpcmo2fxO4DPaekU-CbXYiH9O95f2RqWHUMd1dcNO6s"
+            val intervalo = input_novo_intervalo.text.toString().toInt()
+            val body = NovoIntervalo(intervalo)
+
+            val idLinha = intent.extras?.getInt("idLinha")
+
+
+            if (idLinha != null) {
+                val requests: FiscalApi = HttpHelper().getApiClient()!!.create(FiscalApi::class.java)
+                val resultado = requests.alterarIntervaloViagem(idLinha, body, token)
+
+                resultado.enqueue(object : Callback<Void> {
+                    override fun onFailure(call: Call<Void>, t: Throwable) {
+                        respostaRequisicao.value = false
+                        loading.value = false
+                    }
+
+                    override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                        respostaRequisicao.value = true
+                        loading.value = false
+                    }
+                })
+            } else {
+                respostaRequisicao.value = false
+                loading.value = false
+            }
+        }
+
+        //this.finish()
+    }
+
