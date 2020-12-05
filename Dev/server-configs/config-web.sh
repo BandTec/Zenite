671b91ab06eedf83e4003bdb84b68dd6beecab7f
jPedroSoares <joao.soares204@hotmail.com>
2020-12-05 16:24:26 -0300

    Adicionando permissão a pasta

--- Dev/server-configs/config-web.sh
+++ Dev/server-configs/config-web.sh
@@ -11,5 +11,6 @@ docker run -p 8081:3000 -d web-server
 docker run -p 8082:3000 -d web-server
 
 cd /home/ec2-user/Zenite/Dev/server-configs
+chmod +x *
 docker build -t load-balance .
 docker run -p 80:80 -d load-balance
